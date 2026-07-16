35167847ddd896e6929c81ee87846bad7d13bb27
tiennd <nguyendinhtien012@gmail.com>
2026-07-16 12:43:25 +0700

    power opt

--- .config/hypr/modules/autostart.lua
+++ .config/hypr/modules/autostart.lua
@@ -2,7 +2,7 @@
 hl.on("hyprland.start", function ()
     hl.exec_cmd("uwsm app -- awww-daemon --quiet")
     hl.exec_cmd("uwsm app -- awww img \"$HOME/GDrive_bisync/picture/Saved pics/arch_wallpapers/d_void.png\"")
-    hl.exec_cmd("uwsm app -- easyeffects --gapplication-service")
+    -- hl.exec_cmd("uwsm app -- easyeffects --gapplication-service")
     hl.exec_cmd("uwsm app -- wl-paste --type text --watch cliphist store")
     hl.exec_cmd("uwsm app -- wl-paste --type image --watch cliphist store")
 end)
